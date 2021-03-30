260467d66c084e96233bc58ddfafac2072c896d3
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-03-30 18:24:58 +0200

    Add files via upload

---
+++ upotrebaStreamWriterObjekta/upotrebaStreamWriterObjekta/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+namespace SpremanjeTeksta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+            StreamWriter sw = new StreamWriter(@"D:\My Documents\NOOP\datoteka.txt");
+            sw.WriteLine("Ime: { 0}", ime);
+            sw.WriteLine("Prezime: { 0}", prezime);
+            sw.Close();
+        }
+    }
+}
